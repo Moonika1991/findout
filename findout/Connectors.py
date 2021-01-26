eeb5b1d71927a406007a139d9246240a8e353e5e
Moonika1991 <monika9107@gmail.com>
2021-01-26 09:03:44 +0100

    [FIN2/FIN-4] Create method validate

--- findout/Connectors.py
+++ findout/Connectors.py
@@ -3,16 +3,19 @@ import json
 
 
 def parse(search):
-    pattern = r'(?P<fullmatch>(?P<name>\w[[:alnum:]]*)(?:\((?P<args>(?:[^\(\)]|(?R))*)\))?)'
+    UNQUOTED_NAME = r'(?:\w[[:alnum:]_]*)'
+    QUOTED_NAME = r'(?:\"(?:\\.|[^\\"])*\")'
+    NAME = "(?P<name>%s|%s)" % (UNQUOTED_NAME, QUOTED_NAME)
+    pattern = r'(?P<fullmatch>%s(?:\((?P<args>(?:[^\(\)]|(?R))*)\))?)' % NAME
     p = regex.compile(pattern)
     result = []
     for m in p.finditer(search):
         tmp = m.groupdict()
         if tmp['args'] and "," in tmp['args']:
             tmp['args'] = parse(tmp['args'])
-            result.append({tmp["name"] : tmp['args']})
+            result.append({tmp["name"]: tmp['args']})
         elif "(" in tmp["fullmatch"]:
-            result.append({tmp["name"] : tmp['args']})
+            result.append({tmp["name"]: tmp['args']})
         else:
             result.append(tmp['name'])
     return result
@@ -27,7 +30,7 @@ def validate(search_tree):
         result = validate(edge[fun])
         if not result:
             return False
-        schema = json.load(open("C:\\Users\\monik\\PycharmProjects\\findout\\etc\\functions\\%s.json"%(fun)))
+        schema = json.load(open("C:\\Users\\monik\\PycharmProjects\\findout\\etc\\functions\\%s.json" % (fun)))
         print(schema)
         if int(schema.get('maxNumberOfArguments')) == -1:
             result = True
@@ -40,5 +43,5 @@ def validate(search_tree):
     return result
 
 
-x= parse('or(search(a,”example”),search(b,”another”))  search(c,"code") ')
+x = parse('or(search(a,”example”),search(b,"another example"))  search(c,"code") ')
 print(validate(x))
