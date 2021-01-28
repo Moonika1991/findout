e6d375814800174043394314410a36a4ecae0768
Moonika1991 <monika9107@gmail.com>
2021-01-28 10:55:48 +0100

    Syntactic and semantic validation corrected. Raise exception added.

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 from findout.QueryParser import QueryParser
 
-search = 'source("osdfosfn", "osdfjerjnfg") or(search(a,"example"),search(b,"another example"))  search(c,"code") '
+search = 'source("osdfosfn", "osdfjerjnfg") or(search(a, b),search(b,"another example"))  search(c,"code") '
 
 sc = QueryParser(search)
-print(QueryParser.get_source(sc))
+
 
 
