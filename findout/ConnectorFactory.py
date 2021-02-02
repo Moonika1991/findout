9dd0c3c757f2229359af81ab16a80c6173695982
Moonika1991 <monika9107@gmail.com>
2021-02-02 10:54:16 +0100

    [FIN-8]Create a search class for CSV file

--- findout/ConnectorFactory.py
+++ findout/ConnectorFactory.py
@@ -1,8 +1,16 @@
 from abc import ABC, abstractmethod
+from pathlib import Path
+import json
+from findout.CSVConnector import CSVConnector
+from findout.SQLConnector import SQLConnector
+
 
 
 class ConnectorFactory(ABC):
 
-    @abstractmethod
-    def connector_creator(self):
-        pass
+    def create_connector(self, filename):
+        path = Path(__file__).resolve().parents[1]
+        connector = json.load(open("%s\\etc\\connectors\\%s.json" % (path, filename)))
+        targetclass = connector.get('type').upper() + 'Connector'
+
+        return globals()[targetclass](connector)
